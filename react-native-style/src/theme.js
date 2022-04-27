4d0067fa7f48b41e375bb04104ea858b19cb309e
mooilicht <62236303+mooilicht@users.noreply.github.com>
2022-04-28 06:52:11 +0900

    스타일드 컴포넌트

---
+++ react-native-style/src/theme.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  background: "#ffffff",
+  text: "#ffffff",
+  purple: "#9b59b6",
+  blue: "#3498db",
+};
+
+export const dartTheme = {
+  background: "#34495e",
+  text: "#34495e",
+  purple: "#9b59b6",
+  blue: "#3498db",
+};
